560a2549c532204344846bc4dc9bdc052ca0d7fd
Shivaraj Patil <patilshivaraj65@gmail.com>
2024-02-09 16:16:27 +0530

    first

---
+++ 18-forkify/final/src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = 'fd45e9c0-7dc3-48f0-9656-bf17d4b4d86f';
+export const MODAL_CLOSE_SEC = 2.5;
